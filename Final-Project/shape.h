1450cddaf3f3031da346c45a978a7523d6bb0e0d
tlsdn0403@NoteBook <tlsdn0403@gmail.com>
2024-11-19 15:00:45 +0900

    클래스 헤더파일로 분리중

---
+++ Final-Project/shape.h
@@ -0,0 +1,26 @@
+#ifndef SHAPE_H
+#define SHAPE_H
+#include <iostream>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include<gl/glm/glm/glm.hpp>
+#include<gl/glm/glm/ext.hpp>
+#include<gl/glm/glm/gtc/matrix_transform.hpp>
+#include <cstdlib>
+#include <vector>
+
+class Shape {
+public:
+    virtual void draw(GLuint shaderProgramID, GLuint vbo[]) = 0;
+    virtual void generateFaces() = 0;
+    virtual ~Shape() {}
+
+    GLfloat position[3];
+    GLfloat color[3];
+    GLfloat size;
+    int num;
+    std::vector<std::vector<GLfloat>> faces;
+    std::vector<std::vector<GLfloat>> vertexColors;
+};
+
+#endif // SHAPE_H
