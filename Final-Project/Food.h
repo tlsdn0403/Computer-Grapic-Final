588a6e6f660cf2def14ea41a75003f6519748c0b
usungcode <0513cloud@gmail.com>
2024-12-13 21:08:17 +0900

    Timerfunctoin 함수 추가해서 시간이 지남에 따라 z 증가

--- Final-Project/Food.h
+++ Final-Project/Food.h
@@ -6,9 +6,9 @@
 #include <iostream>
 #include <GL/glew.h>
 #include <GL/freeglut.h>
-#include<gl/glm/glm/glm.hpp>
-#include<gl/glm/glm/ext.hpp>
-#include<gl/glm/glm/gtc/matrix_transform.hpp>
+#include<glm/glm.hpp>
+#include<glm/ext.hpp>
+#include<glm/gtc/matrix_transform.hpp>
 #include <vector>
 
 
