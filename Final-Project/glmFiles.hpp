68dc8b7235c20e63f92470efe3c07d8ac5a20a5f
usungcode <0513cloud@gmail.com>
2024-12-15 15:10:10 +0900

    .

--- Final-Project/glmFiles.hpp
+++ Final-Project/glmFiles.hpp
@@ -1,5 +1,6 @@
 #include <GL/glew.h>
+#include <GL/glut.h>
 #include <GL/freeglut.h>
-#include<glm/glm/glm.hpp>
-#include<glm/glm/ext.hpp>
-#include<glm/glm/gtc/matrix_transform.hpp>
+#include<glm/glm.hpp>
+#include<glm/ext.hpp>
+#include<glm/gtc/matrix_transform.hpp>
